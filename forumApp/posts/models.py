befd6d9dcc8c877dceaab6eef693fa396fa8bbd7
Rafael Petrov <djfanta@abv.bg>
2024-10-09 19:13:36 +0300

    Initial commit

---
+++ forumApp/posts/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+from forumApp.posts.choices import LanguageChoice
+from forumApp.posts.validators import BadLanguageValidator
+
+
+class Post(models.Model):
+    TITLE_MAX_LENGTH = 100
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+    )
+
+    content = models.TextField(
+        validators=(
+            BadLanguageValidator(),
+        )
+    )
+
+    author = models.CharField(
+        max_length=30
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    languages = models.CharField(
+        max_length=20,
+        choices=LanguageChoice.choices,
+        default=LanguageChoice.OTHER,
+    )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    author = models.CharField(
+        max_length=100,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
